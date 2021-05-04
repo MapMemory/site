485b993cd066db48665c3bcd9a7abaa878ae2be7
xihnik <shkadinskiy98@gmail.com>
2021-05-05 02:01:40 +0300

    feat: added new routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Switch, Route } from 'react-router-dom';
 import Preview from './pages/preview.jsx'
+import About from './pages/about.jsx'
+import Download from './pages/downolad.jsx'
+import Privacy from './pages/privacy.jsx'
 
 export default () => {
     return (
         <Switch>
             <Route exact path="/" component={Preview} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/download" component={Download} />
+            <Route exact path="/privacy" component={Privacy} />
         </Switch>
     );
 }
