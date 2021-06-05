2785b040fd7dc7a4648b5234bd20672187c7d1a5
xihnik <shkadinskiy98@gmail.com>
2021-06-05 23:24:26 +0300

    feat: added scroll to top

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import Routes from './routes.js'
-import reportWebVitals from './utils/reportWebVitals';
+
+import ScrollToTop from './utils/helpers/ScrollToTop.jsx'
 
 ReactDOM.render(
   <BrowserRouter history={BrowserRouter}>
+    <ScrollToTop />
     <Routes />
   </BrowserRouter>,
   document.getElementById('root')
 );
-
-reportWebVitals();
