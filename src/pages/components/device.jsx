43cc524ef59fbcb8e969676cca0633ac73a92457
xihnik <shkadinskiy98@gmail.com>
2021-06-06 03:39:11 +0300

    feat: updated page

--- src/pages/components/device.jsx
+++ src/pages/components/device.jsx
@@ -1,15 +1,29 @@
-import '../../styles/light/green/components/device.scss';
+import '../../styles/pages/components/device.scss'
 
-export default ({ title, img, url }) => {
-    return (
-        <div className="block green">
-            <div className="front">
-                <div className="text general">{title}</div>
-                <button onClick={() => { window.location.assign(url); }}>Скачать</button>
+export default ({ left, right, title, ImgObj, url, theme }) => {
+    if (left)
+        return (
+            <div style={theme.backgoundColorFirst} className="block">
+                <div className="front">
+                    <div style={theme.text} className="text general">{title}</div>
+                    <button style={{ backgroundColor: theme.headerColorPrimary.backgroundColor, color: theme.text.color }} onClick={() => { window.location.assign(url); }}>Скачать</button>
+                </div>
+                <div className="back">
+                    <ImgObj fill={theme.svg.fill} className="image-object" alt="" />
+                </div>
             </div>
-            <div className="back">
-                <img className="image-object" src={img} alt="" />
+        );
+
+    if (right)
+        return (
+            <div style={theme.backgoundColorSecond} className="block">
+                <div className="front">
+                    <div style={theme.text} className="text general">{title}</div>
+                    <button style={{ backgroundColor: theme.headerColorPrimary.backgroundColor, color: theme.text.color }} onClick={() => { window.location.assign(url); }}>Скачать</button>
+                </div>
+                <div className="back">
+                    <ImgObj fill={theme.svg.fill} className="image-object" alt="" />
+                </div>
             </div>
-        </div>
-    );
+        );
 }
--- src/pages/downolad.jsx
+++ src/pages/downolad.jsx
@@ -1,55 +1,87 @@
 import React from 'react';
-import '../styles/light/green/download.scss'
+
+import '../styles/pages/download.scss'
 import Header from './components/header.jsx'
 import Footer from './components/footer.jsx'
 import Device from './components/device.jsx';
-import imgAndroid from '../assets/images/svg/android.svg';
-import imgApple from '../assets/images/svg/apple.svg';
-import imgLinux from '../assets/images/svg/linux.svg';
-import imgWindows from '../assets/images/svg/windows.svg';
+
+import { ReactComponent as imgAndroid } from '../assets/images/svg/android.svg';
+import { ReactComponent as imgApple } from '../assets/images/svg/apple.svg';
+import { ReactComponent as imgLinux } from '../assets/images/svg/linux.svg';
+import { ReactComponent as imgWindows } from '../assets/images/svg/windows.svg';
+
+import Person from '../utils/person.jsx';
+import Menu from './components/menu.jsx';
 
 function Body() {
+    let person = new Person();
+
     return (
         <div id="body">
             <div id="Smartphone">
+                <div style={person.getThemeColors().headerColorPrimary} className="name">
+                    <hr />
+                    <p style={{ color: person.getThemeColors().text.color, backgroundColor: person.getThemeColors().headerColorPrimary.backgroundColor }}>Смартфон</p>
+                    <hr />
+                </div>
+
                 <Device
                     title="Android"
-                    img={imgAndroid}
+                    ImgObj={imgAndroid}
                     url="https://github.com/MapMemory/android"
+                    right={true}
+                    theme={person.getThemeColors()}
                 />
                 <Device
                     title="IOS"
-                    img={imgApple}
+                    ImgObj={imgApple}
                     url="https://github.com/MapMemory/ios"
+                    left={true}
+                    theme={person.getThemeColors()}
                 />
             </div>
             <div id="Desktop">
+                <div style={person.getThemeColors().headerColorPrimary} className="name">
+                    <hr />
+                    <p style={{ color: person.getThemeColors().text.color, backgroundColor: person.getThemeColors().headerColorPrimary.backgroundColor }}>Десктоп</p>
+                    <hr />
+                </div>
+
                 <Device
                     title="Linux"
-                    img={imgLinux}
+                    ImgObj={imgLinux}
                     url="https://github.com/MapMemory/desktop"
+                    right={true}
+                    theme={person.getThemeColors()}
                 />
                 <Device
                     title="Windows"
-                    img={imgWindows}
+                    ImgObj={imgWindows}
                     url="https://github.com/MapMemory/desktop"
+                    left={true}
+                    theme={person.getThemeColors()}
                 />
                 <Device
                     title="MacOS"
-                    img={imgApple}
+                    ImgObj={imgApple}
                     url="https://github.com/MapMemory/desktop"
+                    right={true}
+                    theme={person.getThemeColors()}
                 />
             </div>
-        </div>
+        </div >
     );
 }
 
 export default () => {
     return (
-        <div>
+        <div id="download">
             <Header />
-            <Body />
-            <Footer />
+            <Menu />
+            <div id="blockScroll">
+                <Body />
+                <Footer />
+            </div>
         </div>
     );
 };
