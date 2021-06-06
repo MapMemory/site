e0b273cfd4d16db34bc214d980a1bc8c2777b4ae
xihnik <shkadinskiy98@gmail.com>
2021-06-06 03:47:01 +0300

    feat: updated footer

--- src/pages/components/footer.jsx
+++ src/pages/components/footer.jsx
@@ -1,47 +1,53 @@
-import '../../styles/light/green/components/footer.scss'
+import '../../styles/pages/components/footer.scss'
+import { useHistory } from "react-router-dom";
+
+import Person from '../../utils/person.jsx';
 
 export default () => {
+    let person = new Person();
+    const history = useHistory();
+
     return (
-        <div id="footer">
+        <div style={{ backgroundColor: person.getThemeColors().headerColorPrimary.backgroundColor, borderColor: person.getThemeColors().headerColorPrimary.backgroundColor }} id="footer">
             <div className="container">
                 <div className="columns">
                     <div className="group-columns">
-                        <span className="copyright">© 2021 MapMemory.</span>
+                        <span className="copyright" style={person.getThemeColors().text}>© 2021 MapMemory.</span>
                         <br />
-                        <div>Быстро, гладко, просто, чисто - это наша общая цель.</div>
-                        <div>Для связи с нами используйте Email.</div>
+                        <div style={person.getThemeColors().text}>Быстро и просто - это наша общая цель.</div>
+                        <div style={person.getThemeColors().text}>Для связи с нами используйте Email.</div>
                     </div>
                     <div className="column">
-                        <strong>Компания</strong>
+                        <strong style={person.getThemeColors().text}>Компания</strong>
                         <ul>
-                            <li>Блог</li>
-                            <li>Пожертвовать</li>
-                            <li>О нас</li>
-                            <li>Наша политика</li>
+                            <li onClick={() => { history.push("/map"); }} style={person.getThemeColors().text}>Карта</li>
+                            <li onClick={() => { history.push("/themes"); }} style={person.getThemeColors().text}>Палитры</li>
+                            <li onClick={() => { history.push("/about"); }} style={person.getThemeColors().text}>О нас</li>
+                            <li onClick={() => { history.push("/privacy"); }} style={person.getThemeColors().text}>Наша политика</li>
                         </ul>
                     </div>
                     <div className="column">
-                        <strong>Скачать</strong>
+                        <strong style={person.getThemeColors().text}>Скачать</strong>
                         <ul>
-                            <li>Настольный</li>
-                            <li>Телефон</li>
+                            <li onClick={() => { history.push("/download"); }} style={person.getThemeColors().text}>ПК</li>
+                            <li onClick={() => { history.push("/download"); }} style={person.getThemeColors().text}>Смартфон</li>
                         </ul>
                     </div>
                     <div className="column">
-                        <strong>Социальное</strong>
+                        <strong style={person.getThemeColors().text}>Социальное</strong>
                         <ul>
-                            <li>Twitter</li>
-                            <li>GitHub</li>
+                            <li onClick={() => { window.location.assign("https://twitter.com/MapMemoryLink") }} style={person.getThemeColors().text}>Twitter</li>
+                            <li onClick={() => { window.location.assign("https://github.com/MapMemory") }} style={person.getThemeColors().text}>GitHub</li>
                         </ul>
                     </div>
                     <div className="column">
-                        <strong>Помощь</strong>
+                        <strong style={person.getThemeColors().text}>Помощь</strong>
                         <ul>
-                            <li>Служба поддержки</li>
+                            <li onClick={() => window.open('mailto:test@example.com?subject=subject&body=body')} style={person.getThemeColors().text}>Служба поддержки</li>
                         </ul>
                     </div>
                 </div>
             </div>
-        </div>
+        </div >
     );
 }
