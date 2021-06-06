6e55f4e715b53af349bbcc579104ce1324ad6058
xihnik <shkadinskiy98@gmail.com>
2021-06-06 03:55:19 +0300

    feat: added work with server

---
+++ src/utils/constants.jsx
@@ -0,0 +1,15 @@
+const SERVER_ADDRESS = 'http://localhost:80/';
+
+const PAGES = {
+    MAP: {
+        GET_IMG_OF_OBJECT: 'map/getImageOfObject.php',
+        GET_AIM_OBJECT: 'map/getAimObject.php',
+        GET_POINT: 'map/getPoint.php',
+        GET_MONUMENTS: 'map/getMonuments.php',
+        GET_TABLES: 'map/getTables.php',
+        GET_STREETS: 'map/getStreets.php',
+        SET_RATING: 'map/setRating.php'
+    },
+};
+
+export default { SERVER_ADDRESS, PAGES };
---
+++ src/utils/server.jsx
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import CONSTANTS from '../utils/constants.jsx';
+
+export default class {
+    constructor() { }
+
+    async getObjects(name) {
+        return {
+            monuments: await this.getMonuments(name),
+            tables: await this.getTables(name),
+            streets: await this.getStreets(name)
+        };
+    }
+
+    async setRating(aim) {
+        axios.post(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.SET_RATING + `?ip=${await aim.person.information.getIP()}` + `&id=${aim.object.id}` + `&scheme=${aim.object.id_scheme}` + `&rating=${aim.newRating}`);
+    }
+
+    async getImgsOfObject(aim) {
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_IMG_OF_OBJECT + `?scheme=${aim.scheme}` + `&id=${aim.id}`).then((result) => { return result.data });
+    }
+
+    async getAimObject(aim) {
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_AIM_OBJECT + `?scheme=${aim.scheme}` + `&id=${aim.id}`).then((result) => { return result.data });
+    }
+
+    async getPoint(name) {
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_POINT + `?name=${name}`).then((result) => { return result.data });
+    }
+
+    async getMonuments(name) {
+        console.log(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_MONUMENTS + `?name=${name}`);
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_MONUMENTS + `?name=${name}`).then((result) => { return result.data });
+    }
+
+    async getTables(name) {
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_TABLES + `?name=${name}`).then((result) => { return result.data });
+    }
+
+    async getStreets(name) {
+        return await axios.get(CONSTANTS.SERVER_ADDRESS + CONSTANTS.PAGES.MAP.GET_STREETS + `?name=${name}`).then((result) => { return result.data });
+    }
+}
