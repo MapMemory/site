d6ec59ce13a466b7ed4d743aa727f50ed8dcb719
xihnik <shkadinskiy98@gmail.com>
2021-06-06 15:14:13 +0300

    feat: added palettes

---
+++ src/utils/themes/dark.jsx
@@ -0,0 +1,31 @@
+export default class {
+    green() {
+        return {
+            text: { color: "#ffffff" },
+            svg: { fill: "#ffffff" },
+            headerColorPrimary: { backgroundColor: "#313131" },
+            backgoundColorFirst: { backgroundColor: "#519067" },
+            backgoundColorSecond: { backgroundColor: "#4aa96c" }
+        };
+    }
+
+    orange() {
+        return {
+            text: { color: "#ffffff" },
+            svg: { fill: "#ffffff" },
+            headerColorPrimary: { backgroundColor: "#313131" },
+            backgoundColorFirst: { backgroundColor: "#ba6b57" },
+            backgoundColorSecond: { backgroundColor: "#f1935c" }
+        };
+    }
+
+    red() {
+        return {
+            text: { color: "#ffffff" },
+            svg: { fill: "#ffffff" },
+            headerColorPrimary: { backgroundColor: "#313131" },
+            backgoundColorFirst: { backgroundColor: "#9E3C3A" },
+            backgoundColorSecond: { backgroundColor: "#c84a4a" }
+        };
+    }
+}
---
+++ src/utils/themes/light.jsx
@@ -0,0 +1,31 @@
+export default class {
+    green() {
+        return {
+            text: { color: "#000000" },
+            svg: { fill: "#000000" },
+            headerColorPrimary: { backgroundColor: "#ffffff" },
+            backgoundColorFirst: { backgroundColor: "#9fe6a0" },
+            backgoundColorSecond: { backgroundColor: "#8fd9a8" }
+        };
+    }
+
+    orange() {
+        return {
+            text: { color: "#000000" },
+            svg: { fill: "#000000" },
+            headerColorPrimary: { backgroundColor: "#ffffff" },
+            backgoundColorFirst: { backgroundColor: "#FFBE73" },
+            backgoundColorSecond: { backgroundColor: "#FF9373" }
+        };
+    }
+
+    red() {
+        return {
+            text: { color: "#000000" },
+            svg: { fill: "#000000" },
+            headerColorPrimary: { backgroundColor: "#ffffff" },
+            backgoundColorFirst: { backgroundColor: "#ffb4b4" },
+            backgoundColorSecond: { backgroundColor: "#ff9292" }
+        };
+    }
+}
